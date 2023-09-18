51dc7ef6cd7f047ee62ebd1c7c4468716638485d
Matagaro <nyahumphrey@gmail.com>
2023-09-18 12:32:20 +0300

    changed running port to 8000

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use('/image', image);
 
 
  
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
